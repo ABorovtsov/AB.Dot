5ce16d15f4e75e15d5f5f6064ccff395680354b1
Alexander Borovtsov <Alexander.Borovtsov@dataart.com>
2021-09-22 12:50:38 +0300

    DOT (graph description) file generator for tree and graph
    - initial commit with the binary tree and the graph adjacency list dot-writers

---
+++ AB.Dot/BTreeWriter.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace AB.Dot
+{
+    public class BTreeWriter
+    {
+        private readonly string _outputFolderPath;
+
+        public BTreeWriter(string outputFolderPath)
+        {
+            if (string.IsNullOrWhiteSpace(outputFolderPath))
+            {
+                throw new ArgumentException($"{nameof(outputFolderPath)} must be set");
+            }
+
+            _outputFolderPath = outputFolderPath;
+        }
+
+        public async Task WriteDotAsync<TNode>(IEnumerable<TNode> nodes, string fileName="graph.dot")
+        {
+            if (nodes == null) throw new ArgumentNullException(nameof(nodes));
+            if (string.IsNullOrWhiteSpace(fileName)) throw new ArgumentException($"{nameof(fileName)} cannot be null or white space");
+
+            ValidateTNode(typeof(TNode));
+
+            await File.WriteAllLinesAsync(Path.Combine(_outputFolderPath, fileName), GetDotLines(nodes));
+        }
+
+        private void ValidateTNode(Type type)
+        {
+            if (type.GetProperty("Key") == null)
+            {
+                throw new ArgumentException("\"TNode\" mast have the \"Key\" property");
+            }
+
+            if (type.GetProperty("Left") == null)
+            {
+                throw new ArgumentException("\"TNode\" mast have the \"Left\" property");
+            }
+
+            if (type.GetProperty("Right") == null)
+            {
+                throw new ArgumentException("\"TNode\" mast have the \"Right\" property");
+            }
+        }
+
+        private IEnumerable<string> GetDotLines<TNode>(IEnumerable<TNode> nodes)
+        {
+            yield return "digraph G {";
+            
+            foreach (dynamic node in nodes)
+            {
+                var hasRelations = node.Right != null || node.Left != null;
+
+                if (node.Left != null)
+                {
+                    yield return $"    {node.Key} -> {node.Left.Key};";
+                }
+                else if (hasRelations)
+                {
+                    yield return $"    \"{node.Key}LeftNull\" [label=\"NULL\"];";
+                    yield return $"    {node.Key} -> \"{node.Key}LeftNull\";";
+                }
+
+                if (node.Right != null)
+                {
+                    yield return $"    {node.Key} -> {node.Right.Key};";
+                }
+                else if (hasRelations)
+                {
+                    yield return $"    \"{node.Key}RightNull\" [label=\"NULL\"];";
+                    yield return $"    {node.Key} -> \"{node.Key}RightNull\";";
+                }
+
+                if (!hasRelations)
+                {
+                    yield return $"    {node.Key};";
+                }
+            }
+
+            yield return "}";
+        }
+    }
+}
---
+++ AB.Dot/GraphWriter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AB.Dot
+{
+    public class GraphWriter
+    {
+        private readonly string _outputFolderPath;
+        private readonly bool _directed;
+
+        public GraphWriter(string outputFolderPath, bool directed=false)
+        {
+            if (string.IsNullOrWhiteSpace(outputFolderPath))
+            {
+                throw new ArgumentException($"{nameof(outputFolderPath)} must be set");
+            }
+
+            _outputFolderPath = outputFolderPath;
+            _directed = directed;
+        }
+
+        public async Task WriteDotAsync(IEnumerable<IEnumerable<int>> adjacencyList, string fileName="graph.dot")
+        {
+            if (adjacencyList == null) throw new ArgumentNullException(nameof(adjacencyList));
+            if (string.IsNullOrWhiteSpace(fileName)) throw new ArgumentException($"{nameof(fileName)} cannot be null or white space");
+
+            await File.WriteAllLinesAsync(Path.Combine(_outputFolderPath, fileName), GetDotLines(adjacencyList));
+        }
+
+        private IEnumerable<string> GetDotLines(IEnumerable<IEnumerable<int>> adjacencyList)
+        {
+            yield return _directed 
+                ? "strict digraph G {"
+                : "strict graph G {";
+            
+            var indexFrom = 0;
+            foreach (IEnumerable<int> nodeEdges in adjacencyList)
+            {
+                var edgeCount = 0;
+
+                foreach (int indexTo in nodeEdges)
+                {
+                    edgeCount++;
+
+                    yield return _directed
+                        ? $"    {indexFrom} -> {indexTo};"
+                        : $"    {indexFrom} -- {indexTo};";
+                }
+
+                if (edgeCount == 0)
+                {
+                    yield return $"    {indexFrom};";
+                    continue;
+                }
+
+                indexFrom++;
+            }
+
+            yield return "}";
+        }
+    }
+}
