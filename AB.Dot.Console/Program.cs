901bb859d09b29888e2f59315db0341694c39dc1
Alexander Borovtsov <Alexander.Borovtsov@dataart.com>
2021-09-26 18:19:54 +0300

    DOT file generator for tree and graph
    - update documentation and the example project

--- AB.Dot.Console/Program.cs
+++ AB.Dot.Console/Program.cs
@@ -18,8 +18,27 @@ namespace AB.Dot.Console
             await new GraphWriter("./output").WriteDotAsync(graph, "graph.dot");
 
 
+            var node4 = new Node {Key = 4, Left = null, Right = null};
+            var node3 = new Node {Key = 3, Left = null, Right = node4};
+            var node_1 = new Node {Key = -1, Left = null, Right = null};
+            var node0 = new Node {Key = 0, Left = null, Right = null};
+            var node1 = new Node {Key = 1, Left = node_1, Right = node0};
+            var node2 = new Node {Key = 2, Left = node1, Right = node3};
+            
+            var nodes = new List<Node> { node4, node3, node_1, node0, node1, node2 }; // the product of any traversal algorithm 
+            await new BTreeWriter("./output").WriteDotAsync(nodes, "tree.dot");
+
             System.Console.WriteLine("Done!");
             System.Console.ReadLine();
         }
+    }
+
+    public class Node
+    {
+        public int Key { get; set; }
+        
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
     }
 }
